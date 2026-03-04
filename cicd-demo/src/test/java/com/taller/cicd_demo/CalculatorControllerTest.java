899047ddf1fc295d0fda5f12edced9c2987189e7
emanuel <emanuelmorillopajaro@gmail.com>
2026-03-04 14:25:02 -0500

    ci: add initial github actions CI workflow

--- cicd-demo/src/test/java/com/taller/cicd_demo/CalculatorControllerTest.java
+++ cicd-demo/src/test/java/com/taller/cicd_demo/CalculatorControllerTest.java
@@ -1,3 +1,12 @@
+package com.taller.cicd_demo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @WebMvcTest(CalculatorController.class)
  class CalculatorControllerTest {
 
